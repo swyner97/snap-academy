a07e93915fc4eee67ee11eca7b1a18f8997e715a
swyner97 <swyner97@gmail.com>
2023-04-24 13:38:48 -0700

    two ui"

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const toggleSwitch = document.querySelector('#toggle');
 const body = document.querySelector('body');
 const footer = document.querySelector('footer');
+const hero = document.querySelector('.hero');
+const logo = document.querySelector('#logo');
 
 toggleSwitch.addEventListener('change', function() {
   if (this.checked) {
     body.style.backgroundColor = 'white';
     footer.style.backgroundColor = 'black';
+    hero.classList.remove('hero1');
+    hero.classList.add('hero2');
+  
+    logo.src="./assets/images/logo2.png"
+
   } else {
     body.style.backgroundColor = '';
     footer.style.backgroundColor = '';
+    hero.classList.remove('hero2');
+    hero.classList.add('hero1');
+    logo.src="./assets/images/logo.png"
+
   }    
 });
